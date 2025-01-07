76d10dcc839199f74fd5d51e5f6bff0d40657e11
engstk <eng.stk@sapo.pt>
2025-01-07 03:30:57 +0000

    Sign release with debug.keystore
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            signingConfig = signingConfigs.getByName("debug")
         }
 
         debug {
